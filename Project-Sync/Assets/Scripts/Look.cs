e975b5e1f51739db2b526be91692b52ac70d307b
atwe98 <atwe98@gmail.com>
2021-10-15 01:01:04 +0200

    Testing
    Added Look and move script
    Created a map

---
+++ Project-Sync/Assets/Scripts/Look.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform body;
+    public float sensitivity = 100f;
+    private float angle = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float movement_horizontal = Input.GetAxis("Mouse X")*sensitivity*Time.deltaTime;
+        float movement_vertical = -Input.GetAxis("Mouse Y")*sensitivity*Time.deltaTime;
+        angle += movement_vertical;
+        angle = Mathf.Clamp(angle, -90f, 90f);
+        body.Rotate(Vector3.up * movement_horizontal);
+
+        transform.localRotation = Quaternion.Euler(angle,0f,0f);
+    }
+}
---
+++ Project-Sync/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CharacterController controller;
+    public float speed = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 translate = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(translate * speed * Time.deltaTime);
+
+        
+
+    }
+}
