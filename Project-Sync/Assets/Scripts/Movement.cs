c859b8933c138fb388209bdbd228a16c745507af
atwe98 <atwe98@gmail.com>
2021-10-16 00:38:04 +0200

    Cleaned some code

--- Project-Sync/Assets/Scripts/Movement.cs
+++ Project-Sync/Assets/Scripts/Movement.cs
@@ -7,52 +7,49 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
 
     public CharacterController controller;
-    public Transform groundCheck;
-    public float groundDistance= 0.1f;
-    public LayerMask groundMask;
     public float speed = 5f;
     public float gravity = -9.81f;
-    public Vector3 velocity;
+    Vector3 velocity;
     public float jumpheight;
-    public float movement;
-    Vector3 plane = new Vector3(1f, 0f, 1f);
-    public Vector3 test;
-    public bool test2;
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 translate;
+
+
+        // Reseting velocity when touching the ground
         if (controller.isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
+        // Check if jumping and compute upward force
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
             velocity.y += Mathf.Sqrt(-2f * gravity * jumpheight);
         }
-        Vector3 translate = Vector3.zero;
         velocity.y += gravity * Time.deltaTime;
-        test = controller.velocity;
+
+        // Calculate movement according to input if grounded
         if (controller.isGrounded)
         {
             translate = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
             translate *= speed;
 
         }
+        // Maintain horizontal speed when in air
         else
         {
             translate = controller.velocity;
             translate.y = 0;
             
         }
+
+        // Excute the move
         translate.y = velocity.y;
         controller.Move(translate *  Time.deltaTime);
-        test2 = controller.isGrounded;
         
 
     }
